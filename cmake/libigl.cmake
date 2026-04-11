c31c1138fca4195fe3a5c4204f872b230f44f1be
Alon Sardas <alonsardas@gmail.com>
2026-04-11 19:25:39 +0300

    Bumping CMake and libigl versions and adding gtest

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -7,6 +7,6 @@ message(STATUS "Finding/Downloading libigl...")
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG v2.4.0
+    GIT_TAG v2.6.0
 )
 FetchContent_MakeAvailable(libigl)
